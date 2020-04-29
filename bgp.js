eaf434d96da2524e09abc4a6a54b510443442957
Michal Pawluczuk <mpawlucz@gmail.com>
2020-04-29 13:55:05 +0200

    fetch supported stocks from github pages csv, mark stock

--- bgp.js
+++ bgp.js
@@ -1,3 +1,31 @@
-/*
-    TODO
- */
+let APP = {
+    providers: {
+        revolut: {}
+    }
+};
+
+$.ajax({
+    type: "GET",
+    url: "https://mpawlucz.github.io/revolut-stocks-list-raw/revolut-stocks-list.csv",
+    dataType: "text",
+    success: function(responseText){
+        let data = $.csv.toArrays(responseText);
+        for (let i=1; i<data.length; ++i){ // skip first line (csv headers)
+            let item = data[i];
+            APP.providers.revolut[item[0].toUpperCase()] = {
+                ticker: item[0].toUpperCase(),
+                name: item[1]
+            };
+        }
+        // console.log(APP.revolut);
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type === "content-bgp"){
+        if (request.action === "getProviders"){
+            sendResponse({response: "bgp-content", providers: APP.providers});
+            return;
+        }
+    }
+});
--- content_webull.js
+++ content_webull.js
@@ -1,1 +1,63 @@
-console.log('content_webull start');
+(function(global){
+    "use strict";
+    console.log('content_webull start');
+
+    let APP = {
+        providers: {
+            revolut: {}
+        }
+    };
+
+    chrome.runtime.sendMessage({type: "content-bgp", action:'getProviders'}, function(response){
+        if (typeof response == 'undefined'){
+            alert('error: ' + chrome.runtime.lastError);
+            return;
+        }
+        console.log(response.providers);
+        APP.providers = response.providers;
+    });
+
+    function isRevolutSupport(ticker){
+        return APP.providers.revolut.hasOwnProperty(ticker.toUpperCase());
+    }
+
+    if (!("MutationObserver" in global)) {
+        global.MutationObserver = global.WebKitMutationObserver || global.MozMutationObserver;
+    }
+
+    $(document).find('head').append("<style>.janusz-revo {color:yellow}</style>");
+
+    function createRevoMark(){
+        let $revoMark = $("<span> REVO</span>");
+        $revoMark.addClass("janusz-revo");
+        return $revoMark;
+    }
+
+    let observer = new MutationObserver(function (mutationsList) {
+        mutationsList.forEach(mutation => {
+            if(mutation.type == 'childList') {
+                Array.prototype.forEach.call(mutation.target.children, function(child) {
+                    if (child.tagName == "TR"){
+                        let $td = $(child).find("td:nth-child(2)");
+                        if (isRevolutSupport($td.text().trim())){
+                            if ($td.attr('janusz-stock') != "1"){
+                                $td.attr('janusz-stock', "1");
+                                $td.append(createRevoMark());
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    observer.observe(document, {
+        attributes: false,
+        characterData: false,
+        childList: true,
+        subtree: true,
+        attributeOldValue: false,
+        characterDataOldValue: false
+    });
+
+})(window);
